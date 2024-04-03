f2f92e38d8531738744528184f433a5cc7e0889b
AndreyAristov1 <93186946+AndreyAristov1@users.noreply.github.com>
2024-04-03 12:03:52 +0400

    GetTest

---
+++ src/test/java/HomeWork/TestGET.java
@@ -0,0 +1,16 @@
+package HomeWork;
+
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.Test;
+
+public class TestGET {
+
+    @Test
+    public void testHelloWorld(){
+        Response response = RestAssured
+                .get("https://playground.learnqa.ru/api/get_text")
+                .andReturn();
+        response.prettyPrint();
+    }
+}
