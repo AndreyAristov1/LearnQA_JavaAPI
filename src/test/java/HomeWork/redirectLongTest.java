990f7a1185d8020c6f81d17bd7fa2a26ed0febe3
AndreyAristov1 <93186946+AndreyAristov1@users.noreply.github.com>
2024-04-10 19:51:20 +0400

    Второй урок/Домашнее задагие Ex6

---
+++ src/test/java/HomeWork/redirectLongTest.java
@@ -0,0 +1,35 @@
+package HomeWork;
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.Test;
+
+
+public class redirectLongTest {
+
+    @Test
+    public void testLongRedirect() {
+        String location = "https://playground.learnqa.ru/api/long_redirect";
+        for(int i = 0; ;i ++){
+
+        Response response = RestAssured
+                .given()
+                .redirects()
+                .follow(false)
+                .when()
+                .get(location)
+                .andReturn();
+
+        response.prettyPrint();
+
+        location = response.getHeader("Location");
+        System.out.println("URL по которому осуществляется переход " + location);
+        int statusCode = response.statusCode();
+            System.out.println("Код ответа " + statusCode);
+        if(statusCode ==200)break;
+
+
+
+
+        }
+    }
+}
--- src/test/java/HomeWork/redirectTest.java
+++ src/test/java/HomeWork/redirectTest.java
@@ -1,35 +1,27 @@
 package HomeWork;
+
 import io.restassured.RestAssured;
 import io.restassured.response.Response;
 import org.junit.jupiter.api.Test;
-
 
 public class redirectTest {
 
     @Test
     public void testRedirect() {
-        String location = "https://playground.learnqa.ru/api/long_redirect";
-        for(int i = 0; ;i ++){
-
-        Response response = RestAssured
-                .given()
-                .redirects()
-                .follow(false)
-                .when()
-                .get(location)
-                .andReturn();
-
-        response.prettyPrint();
-
-        location = response.getHeader("Location");
-        System.out.println("URL по которому осуществляется переход " + location);
-        int statusCode = response.statusCode();
-            System.out.println("Код ответа " + statusCode);
-        if(statusCode ==200)break;
 
 
+            Response response = RestAssured
+                    .given()
+                    .redirects()
+                    .follow(false)
+                    .when()
+                    .get( "https://playground.learnqa.ru/api/long_redirect")
+                    .andReturn();
 
+            response.prettyPrint();
+
+           String location = response.getHeader("Location");
+            System.out.println("URL по которому осуществляется переход " + location);
 
         }
-    }
 }
