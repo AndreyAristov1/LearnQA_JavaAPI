fd5763bf7a99d7c5e9a0c02c58ee2c7575dba168
AndreyAristov1 <93186946+AndreyAristov1@users.noreply.github.com>
2024-04-10 19:31:12 +0400

    Второй урок/Домашнее задагие Ex7

--- src/test/java/HomeWork/redirectTest.java
+++ src/test/java/HomeWork/redirectTest.java
@@ -8,19 +8,28 @@ public class redirectTest {
 
     @Test
     public void testRedirect() {
-
+        String location = "https://playground.learnqa.ru/api/long_redirect";
+        for(int i = 0; ;i ++){
 
         Response response = RestAssured
                 .given()
                 .redirects()
                 .follow(false)
                 .when()
-                .get("https://playground.learnqa.ru/api/long_redirect")
+                .get(location)
                 .andReturn();
 
         response.prettyPrint();
 
-        String location = response.getHeader("Location");
-        System.out.println(location);
+        location = response.getHeader("Location");
+        System.out.println("URL по которому осуществляется переход " + location);
+        int statusCode = response.statusCode();
+            System.out.println("Rjl код ответа " + statusCode);
+        if(statusCode ==200)break;
+
+
+
+
+        }
     }
 }
