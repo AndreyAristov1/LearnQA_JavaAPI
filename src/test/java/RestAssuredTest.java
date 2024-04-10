df77ababae0ffbcaed5c138462f0986057b7af06
AndreyAristov1 <93186946+AndreyAristov1@users.noreply.github.com>
2024-04-10 10:36:40 +0400

    Второй урок

---
+++ src/test/java/RestAssuredTest.java
@@ -0,0 +1,31 @@
+import  io.restassured.RestAssured;
+import io.restassured.path.json.JsonPath;
+import org.junit.jupiter.api.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class RestAssuredTest {
+
+    @Test
+    public void testRestAssured() {
+//Создаём переменную для передачи нескольких параметров
+        Map<String, String> params = new HashMap<>();
+        params.put("name", "John");
+        //Создаём переменную в которой будет храниться полученный ответ и парсим в формат json
+        JsonPath response = io.restassured.RestAssured
+                .given() //говорит что далее будет передача параметров
+                // .queryParam("name", "John") //Передаём нужные параметры
+                .queryParams(params)
+                .get("https://playground.learnqa.ru/api/hello")
+                .jsonPath();
+
+        String answer = response.get("answer");
+
+        if (answer == null) {
+            System.out.println("Такого ключа нет");
+        } else {
+            System.out.println(answer);
+        }
+    }
+}
