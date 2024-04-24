0b1f1b35290d6fcb8126bac0bb2e21daa73d5182
AndreyAristov1 <93186946+AndreyAristov1@users.noreply.github.com>
2024-04-24 11:19:35 +0400

    Последний урок. Тесты на проверку создания нового пользователя

--- src/test/java/lib/Assertions.java
+++ src/test/java/lib/Assertions.java
@@ -11,5 +11,23 @@ public class Assertions {
 
         int value = Response.jsonPath().getInt(name);
         assertEquals(expectedValue, value, "JSON не равено ожидаемому значению");
+
+
+    }
+    public static void assertResponseTextEquals(Response Response, String expectedAnswer){
+        assertEquals(expectedAnswer,
+                Response.asString(),
+                "Текст ответа не соответствует ожидаемому");
+    }
+
+    public static void assertResponseCodeEquals(Response Response, int expectedStatusCode){
+        assertEquals(expectedStatusCode,
+                Response.statusCode(),
+                "Статус код ответа не соответствует ожидаемому");
+    }
+
+    public static void assertJsonHasKey(Response Response, String expectedFieldName){
+
+        Response.then().assertThat().body("$", hasKey(expectedFieldName));
     }
 }
